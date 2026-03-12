95ad0f9f7333555f974a73929b02cc71eb1f2d6a
Admin <tigrulichka00@gmail.com>
2026-03-12 23:21:09 +0700

    2

---
+++ app/src/main/java/com/example/playlistmaker6/SettingsActivity.kt
@@ -0,0 +1,21 @@
+package com.example.playlistmaker6
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val backButton = findViewById<ImageButton>(R.id.back_button)
+        backButton.setOnClickListener {
+            finish() // 👈 Просто закрываем этот экран — возвращаемся к MainActivity
+        }
+    }
+}
